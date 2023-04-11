b5c6dd567d5258709c03f23251ad0a0a6d264f1d
silence_zhpf <silence_zhpf@aliyun.com>
2023-04-11 17:33:13 +0800

    feat: ✨ 添加主题设置
    resolve #4

--- src/components/ToolBar/ToolBar.tsx
+++ src/components/ToolBar/ToolBar.tsx
@@ -1,9 +1,11 @@
 import styles from "./index.module.css";
 import { LangSwitcher } from "../LangSwitcher";
+import { ThemeSwitcher } from "../ThemeSwitcher";
 
 export function ToolBar(): JSX.Element {
   return (
     <div className={styles.container}>
+      <ThemeSwitcher />
       <LangSwitcher />
     </div>
   );
