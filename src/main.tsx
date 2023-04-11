a31a674db0f2e933db58c25100290649f0bd736d
silence_zhpf <silence_zhpf@aliyun.com>
2023-04-11 17:05:14 +0800

    feat: ✨ 添加模式随系统改变
    resolve: #5

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import "./theme.css";
+import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
